cdb45c7751143eaceb14d176dc69b77c66fde2d2
bbdoggett3 <bbdoggett3@gmail.com>
2020-06-24 17:09:27 -0600

    create graph through props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import './App.css';
-import Chart from './components/Chart'; 
+import React, { Component } from "react";
+import "./App.css";
+import Chart from "./components/Chart";
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
 
-  return (
-    <div className="App">
-      <Chart />
-    </div>
-  );
+    this.state = {
+      chartData: {}
+    }
+  }
+
+  componentWillMount() {
+    this.getChartData();
+  }
+
+  getChartData(){
+    //AXIOS CALL WOULD TYPICALLY GO HERE TO BRING IN THAT DATA.
+    this.setState({
+      chartData: {
+        labels: ['Affirmation', 'Gifts', 'Quality Time', 'Service', 'Touch'],
+            datasets: [
+                {
+                    label: 'Population',
+                    data: [
+                        4,
+                        2,
+                        2,
+                        5,
+                        1
+                    ],
+                    backgroundColor: [
+                        'rgba(0, 198, 248, 0.6)',
+                        'rgba(0, 155, 245, 0.6)',
+                        'rgba(246, 142, 206, 0.6)',
+                        'rgba(230, 85, 174, 0.6)',
+                        'rgba(255, 10, 160, 0.6)'
+                    ]
+                }
+            ]
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Chart chartData={this.state.chartData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,32 +2,11 @@ import React, { Component } from "react";
 import { Pie } from "react-chartjs-2";
 
 class Chart extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-        chartData: {
-            labels: ['Affirmation', 'Gifts', 'Quality Time', 'Service', 'Touch'],
-            datasets: [
-                {
-                    label: 'Population',
-                    data: [
-                        4,
-                        2,
-                        2,
-                        5,
-                        1
-                    ],
-                    backgroundColor: [
-                        'rgba(0, 198, 248, 0.6)',
-                        'rgba(0, 155, 245, 0.6)',
-                        'rgba(246, 142, 206, 0.6)',
-                        'rgba(230, 85, 174, 0.6)',
-                        'rgba(255, 10, 160, 0.6)'
-                    ]
-                }
-            ]
-        }
+        chartData: this.props.chartData
     };
   }
 
