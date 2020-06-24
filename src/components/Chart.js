ae09994ee49de9945b07dbdcbeed66c9c2959aa3
bbdoggett3 <bbdoggett3@gmail.com>
2020-06-24 16:08:47 -0600

    create chart

---
+++ src/components/Chart.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Pie } from "react-chartjs-2";
+
+class Chart extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+        chartData: {
+            labels: ['Affirmation', 'Gifts', 'Quality Time', 'Service', 'Touch'],
+            datasets: [
+                {
+                    label: 'Population',
+                    data: [
+                        4,
+                        2,
+                        2,
+                        5,
+                        1
+                    ],
+                    backgroundColor: [
+                        'rgba(0, 198, 248, 0.6)',
+                        'rgba(0, 155, 245, 0.6)',
+                        'rgba(246, 142, 206, 0.6)',
+                        'rgba(230, 85, 174, 0.6)',
+                        'rgba(255, 10, 160, 0.6)'
+                    ]
+                }
+            ]
+        }
+    };
+  }
+
+  render() {
+    return (
+      <div className="chart">
+        <Pie
+          data={this.state.chartData}
+          options={{maintainAspectRatio: false}}
+        />
+      </div>
+    );
+  }
+}
+
+export default Chart;
